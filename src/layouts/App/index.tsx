fc0e29dfc16be111ccdfd402187eca49690ae16a
leeyoonseo <okayoon.lee@gmail.com>
2022-11-14 22:36:41 +0900

    구상 중

---
+++ src/layouts/App/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header, Container, Footer } from './styles';
+
+const App = () => {
+  return (
+    <div>
+      <Header>header</Header>
+      <Container>Content</Container>
+      <Footer>&copy; Mohaz</Footer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/layouts/App/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  background: red;
+`;
+
+export const Container = styled.div`
+  background: blue;
+`;
+
+export const Footer = styled.footer`
+  background: green;
+`;

392c8b37e4823196378e43338aa1672114112d43
leeyoonseo <okayoon.lee@gmail.com>
2022-11-15 22:31:34 +0900

    작업 방식 구상 중 테스트 이미지 추가

--- src/layouts/App/index.tsx
+++ src/layouts/App/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
-import { Header, Container, Footer } from './styles';
+import { AppWrap, Header, Container, Footer } from './styles';
 
 const App = () => {
   return (
-    <div>
-      <Header>header</Header>
-      <Container>Content</Container>
+    <AppWrap>
+      <Header>
+        <h1 className="header__title">
+          {/* TODO: href */}
+          <a href="#">Mohaz</a>
+        </h1>
+      </Header>
+      <Container>
+        <div className="section">
+          <div className="image__wrap org">
+           <img className="image" src={`${process.env.PUBLIC_URL}/cat.jpg`} alt="cat"/>
+           <img className="image-line" src={`${process.env.PUBLIC_URL}/test-line.png`} alt="cat-line"/>
+          </div>
+        </div>
+        <div className="test__box"></div>
+        <div className="test__box"></div>
+        <div className="test__box"></div>
+      </Container>
       <Footer>&copy; Mohaz</Footer>
-    </div>
+    </AppWrap>
   )
 }
 
--- src/layouts/App/styles.tsx
+++ src/layouts/App/styles.tsx
@@ -1,13 +1,62 @@
 import styled from 'styled-components';
 
+export const AppWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
 export const Header = styled.header`
   background: red;
+  height: 48px;
+  padding: 0 2%;
+  display: flex;
+  align-items: center;
+
+  .header__title {
+    font-size: 24px;
+
+    a { 
+      text-decoration: none;
+      color: #333;
+    }
+  }
 `;
 
 export const Container = styled.div`
   background: blue;
+  padding-top: 48px;
+  height: 100%;
+  box-sizing: border-box;
+
+  .section {
+    /* background: green; */
+    /* background: #fff; */
+    height: 100%;
+
+    .image__wrap {
+      position: relative;
+
+      
+      .image {
+
+      }
+
+      .image-line {
+        clip-path: inset(70% 0 0 0);
+
+      }
+    }
+  }
+
+  .test__box {
+    height: 500px;
+    background: yellow;
+    margin-bottom: 10px;
+  }
 `;
 
 export const Footer = styled.footer`
   background: green;
+  display: none;
 `;
